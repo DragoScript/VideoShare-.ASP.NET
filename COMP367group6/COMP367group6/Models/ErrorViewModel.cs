eac0c09f07ce3620db21ac11e2ed0ccdeb20f9df
DragoScript <94208742+DragoScript@users.noreply.github.com>
2024-12-03 12:55:44 -0500

    switch from Razor to MVC

---
+++ COMP367group6/COMP367group6/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace COMP367group6.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
